e4b5bdbb1450d6bcafe29d0f5ad05a69eeb48ddc
Sergey Dolgunov <dolgunov.s@gmail.com>
2022-06-27 17:29:39 +0300

    add Example 101 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 42, 54, 61, 72, 18 };
+
+int n = array.Length; // длина массива
+
+int find = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //прервать. Когда нашли первый индекс, цикл окончится.
+    }
+    index++;
+}
